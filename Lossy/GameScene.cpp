4bc3c140b2c41838a16167dae4f4901274c372a5
steamb23 <steamb23@gmail.com>
2016-11-28 03:28:46 +0900

    캐릭터 스프라이트 애니메이션 테스트

--- Lossy/GameScene.cpp
+++ Lossy/GameScene.cpp
@@ -1,12 +1,14 @@
 #include "stdafx.h"
 #include "GameScene.h"
 #include "LossyGame.h"
-
+#include "Resource.h"
 
 GameScene::GameScene(std::shared_ptr<Game> game)
     : Scene(game)
 {
     back = std::make_shared<Background>(GetGame());
+    auto sprite = std::make_shared<Sprite>(DrawManager::Inst()->LoadBitmapFromResource(IDB_PLAYER), std::vector<RECT>({ {0,0,128,128},{128,0,128,128} }));
+    characterTest = std::make_shared<SpriteAnimation>(sprite, 2);
 }
 
 GameScene::~GameScene()
@@ -16,9 +18,11 @@ GameScene::~GameScene()
 void GameScene::Update()
 {
     back->Update();
+    characterTest->Update();
 }
 
 void GameScene::Draw()
 {
     back->Draw();
+    characterTest->Draw(100,300);
 }
--- Lossy/GameScene.h
+++ Lossy/GameScene.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Scene.h"
 #include "Background.h"
+#include "SpriteAnimation.h"
 class GameScene :
     public Scene
 {
     std::shared_ptr<Background> back;
+    std::shared_ptr<SpriteAnimation> characterTest;
 public:
     GameScene(std::shared_ptr<Game> game);
     virtual ~GameScene();
