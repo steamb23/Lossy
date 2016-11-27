fe3a44d335becfaf9bd321aff2614cd3c52aec03
steamb23 <steamb23@gmail.com>
2016-11-28 03:20:01 +0900

    리소스 추가 및 수정

--- Lossy/LossyGame.cpp
+++ Lossy/LossyGame.cpp
@@ -21,7 +21,7 @@ void LossyGame::Initialize()
 {
     Game::Initialize();
     this->GetSceneManager()->ChangeScene(std::make_shared<GameScene>(shared_from_this()));
-    isFrameLimit = false;
+    // isFrameLimit = false;
     testBitmap = DrawManager::Inst()->LoadBitmapFromResource(IDB_BITMAPTEST);
     spa = std::make_shared< SpriteAnimation>(std::make_shared<Sprite>(DrawManager::Inst()->LoadBitmapFromResource(IDB_BULLET), std::vector<RECT>({ { 0,0,32,32 },{ 32,0,32,32 } })),0);
 }
